b71049c30e2d26d2f4c470db538bc5633556f5ed
Lisa Canini <three_nineteen@yahoo.com>
2018-11-07 19:31:06 -0800

    added rating field to model

--- books/models.py
+++ books/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from uuid import uuid4
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
 class Books(models.Model):
@@ -7,3 +9,6 @@ class Books(models.Model):
     title = models.CharField(max_length = 50)
     author = models.CharField(max_length = 50)
     date = models.DateTimeField(auto_now=True)
+    rating = models.IntegerField(default=3,
+            validators=[MaxValueValidator(5),
+            MinValueValidator(0)])
