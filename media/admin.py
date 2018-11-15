9d3955ccbd04279b1a624ddcf2f7dfffcd2ce96b
Lisa Canini <three_nineteen@yahoo.com>
2018-11-14 21:10:02 -0800

    day 4 complete

--- media/admin.py
+++ media/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Media, Book, Music, Movie, Reviewer
 # Register your models here.
 class MediaAdmin(admin.ModelAdmin):
-	list_display = ('title', 'artist', 'year_pub', 'rating')
+	list_display = ('title', 'artist', 'year_pub', 'rating', 'reviewer')
 	readonly_fields=('created_at', 'last_modified')
 
 class BookAdmin(admin.ModelAdmin):
